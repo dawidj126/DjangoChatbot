fb02f4c0096a0226682c913f809d4aa93818f4f1
dawidj126 <dawidj126@gmail.com>
2023-06-09 21:19:00 +0200

    first commit

---
+++ core/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import openai
+
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Chat
+
+from django.utils import timezone
+
+openai_api_key = 'your-api-key'
+openai.api_key = openai_api_key
+
+def ai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt = message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature = 0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
+# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect('login')
+    chats = Chat.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ai(message)
+
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
+        return JsonResponse({
+            'message': message,
+            'response': response
+        })
+    return render(request, 'index.html', {
+        'chats': chats
+    })
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid user. Try again.'
+            return render(request, 'login.html', {
+                'error_message': error_message
+            })
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('index')
+            except:
+                error_message = 'Error creating an account'
+                return render(request, 'register.html', {
+                'error_message': error_message
+            })
+        else:
+            error_message = "Those passwords didn't match. Try again."
+            return render(request, 'register.html', {
+                'error_message': error_message
+            })
+    else:
+        return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
